b6ab0f80ba958cc928ceb3006ee01f027c8ad673
Nathanael <Nathanael@Nathanaels-MacBook-Air.local>
2017-05-19 10:16:14 -0500

    practice writing c++ code in RStudio

--- src/test.cpp
+++ src/test.cpp
@@ -1,10 +1,27 @@
+using namespace Rcpp;
+using std::cout;
+
+#include <iostream>
 #include <Rcpp.h>
-using namespace Rcpp
-using namespace std
+
 
 // This is a simple example of exporting a C++ function to R.
 // Source this function into an R session with 
 // Rcpp::sourceCpp [function]
+
+
+// [[Rcpp::export]]
+int main() {
+  for (int hashNum = 1; hashNum <= 5; hashNum++) {
+    cout << "#";
+  }
+  cout << "\n";
+  return 0;
+}
+
+/*** R
+main()
+  */
 
 
 NumericVector timesTwo(NumericVector x) {

5dfe082321d05403f6247de11879909e8823e8e7
Nathanael <Nathanael@Nathanaels-MacBook-Air.local>
2017-05-19 15:53:32 -0500

    .

--- test.R
+++ test.R
@@ -1,8 +1,23 @@
-# Load Library
+install.packages("benchmark")
+install.packages("microbenchmark")
+install.packages("ggplot2")
+# Load Libraries
 library('rbenchmark')
+library('microbenchmark')
+library('ggplot2')
 
 set.seed(1)
 x = rnorm(2^8)
 
 # Run benchmark
 out = benchmark(dwt(x), dwt.BrickWall(x))
+# Table Object
+out
+
+# Run microbenchmark
+out = microbenchmark(dwt(x), dwt.BrickWall(x))
+# Table Object
+summary(out)
+# Violin Plot
+autoplot(out)
+

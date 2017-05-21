be1e2e6a7cc745e585392440c6e1ad4aa1555b55
jjlee13 <jjlee13@adms-iMac.local>
2017-05-21 18:22:14 -0500

    Relection case may be wrong (have not tested). Need clarification of concept

--- test_modwt.R
+++ test_modwt.R
@@ -9,7 +9,7 @@ sourceCpp("src/dwt.cpp")
 source("src/modwt.R")
 
 set.seed(1)
-x = rnorm(100)
+x = rnorm(10000)
 
 
 # Test for equality 
@@ -17,5 +17,25 @@ a = modwt(x, nlevels = 2)
 b = modwt_bw(x, nlevels = 2)
 d = modwt_test(x, nlevels = 2)
 
-d = modwt(x, boundary = "reflection", nlevels = 5)
-e = modwt_bw(x, boundary = "reflection", nlevels = 5)
+all.equal(b, d)
+# Result: Equal except for class name 
+
+
+# Run benchmark
+out = benchmark(modwt_bw(x), modwt_test(x))
+out
+# Results: 
+# Note that modwt_test(x) runs relatively faster than modwt_bw(x) 
+# Some significant relative difference. In terms of bigger datasets this may be hugely beneficial
+
+
+# Run microbenchmark
+out = microbenchmark(modwt_bw(x), modwt_test(x))
+# Violin Plot
+autoplot(out)
+
+
+
+# Tests for "reflection" 
+# d = modwt(x, boundary = "reflection", nlevels = 5)
+# e = modwt_bw(x, boundary = "reflection", nlevels = 5)

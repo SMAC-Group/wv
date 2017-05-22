b049ebf2ff5a737ddcb904ba84dab9d7c2b04580
Justin Lee <jjlee13@illinois.edu>
2017-05-22 14:46:17 -0500

    Can download using devtools::install.github("SMAC-Group")

--- download_package.R
+++
@@ -1,8 +0,0 @@
-# Set working directory to where zip file is downloaded 
-unzip("gmwm2-master.zip")
-file.rename("gmwm2-master", "gmwm2")
-shell("R CMD build gmwm2")
-# Install local gmwm2 package
-install.packages("gmwm2_1.0.tar.gz", repos = NULL)
-# Call gmwm2 Package
-library(gmwm2)

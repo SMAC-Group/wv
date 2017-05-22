73ffef874db4aa759aa3b27096c4bff121d8e522
Justin Lee <jjlee13@illinois.edu>
2017-05-22 03:40:05 -0500

    namespace..6

--- R/dwt.R
+++ R/dwt.R
@@ -45,7 +45,7 @@ dwt = function(x, nlevels = floor(log2(length(x))), filter = "haar", boundary="p
     }
   }
   
-  out = dwt_cpp(x = x, filter_name = filter, nlevels, boundary = boundary, brickwall = bw)  # call to C++ version of dwt
+  out = .Call('gmwm2_dwt_cpp', PACKAGE = gmwm2, x = x, filter_name = filter, nlevels, boundary = boundary, brickwall = bw)  # call to C++ version of dwt
   names(out) = paste0("S",1:nlevels)
   mostattributes(out) = list(J=nlevels, filter = filter, boundary = boundary, brick.wall = bw, class=c("dwt","list"))
   out

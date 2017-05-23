70a8b4264cf2da6cc4091f4e1364ac7c8cdb740c
Justin Lee <munsheet93@gmail.com>
2017-05-23 16:21:36 -0500

    add cross product functions for spatial wv

--- src/inline_functions.h
+++ src/inline_functions.h
@@ -23,4 +23,21 @@ inline double square(double x){
   return x*x;
 }
 
+inline arma::mat crossprod(const arma::mat& x){
+    return x.t() * x;
+}
+
+inline arma::mat crossprod(const arma::mat& x, const arma::mat& y){
+    return x.t() * y;
+}
+
+
+inline arma::mat tcrossprod(const arma::mat& x){
+    return x * x.t();
+}
+
+inline arma::mat tcrossprod(const arma::mat& x, const arma::mat& y){
+    return x * y.t();
+}
+
 #endif

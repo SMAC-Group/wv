e4388b07c9fa8f55e9b6e5f33f839b6abaf4fb0d
Justin Lee <jjlee13@illinois.edu>
2017-05-25 00:51:44 -0500

    delete spatial related files

--- src/psi_functions.h
+++
@@ -1,6 +0,0 @@
-#ifndef PSI_FUNC_H
-#define PSI_FUNC_H
-arma::vec psi_tuk(const arma::vec& x, double sig2_bw, double crob_bw);
-arma::vec der_psi_tuk(const arma::vec& x, double sig2_bw, double crob_bw);
-double der_psi_tuk(double x, double sig2_bw, double crob_bw);
-#endif
--- src/utility_functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILITY_FUNCTIONS_H
-#define UTILITY_FUNCTIONS_H
-arma::mat make_wv_iso(arma::mat wv, int min_dim);
-arma::mat subset_matrix_parallel(const arma::mat& x, const arma::uvec y);
-arma::umat index_mat(int m, int n, std::string ftype = "col");
-arma::mat subset_matrix_parallel(const arma::mat& x, const arma::uvec y);
-arma::vec lower_tri_elem(const arma::mat& X);
-#endif

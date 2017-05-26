1c82a53a2906e9f0318979703033cd046177d110
Justin Lee <jjlee13@illinois.edu>
2017-05-26 13:01:52 -0500

    Add plot to namespace

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -9,7 +9,7 @@
 #' @return y A \code{field<vec>} that contains the wavelet coefficients for each decomposition level
 #' @details
 #' Performs a level J decomposition of the time series using the pyramid algorithm
-#' @author JJB, Justin 
+#' @author James Balamuta and Justin Lee
 #' @keywords internal
 #' @examples
 #' set.seed(999)
@@ -30,7 +30,7 @@ dwt_cpp <- function(x, filter_name, nlevels) {
 #' Performs a level J decomposition of the time series using the pyramid algorithm.
 #' Use this implementation to supply custom parameters instead of modwt(x),
 #' which serves as a wrapper function.
-#' @author JJB, Justin 
+#' @author James Balamuta and Justin Lee
 #' @keywords internal
 #' @examples
 #' set.seed(999)

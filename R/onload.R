b5a9e57c9af2589f988bc466d3c6324574b44707
Justin Lee <jjlee13@illinois.edu>
2017-05-29 04:47:50 -0500

    remove onload.R that prints message when loading package

--- R/onload.R
+++
@@ -1,8 +0,0 @@
-#' @importFrom utils packageDescription
-.onAttach = function(...) {
-  
-  local_version = utils::packageDescription('wv')
-  
-  packageStartupMessage('Version: ', local_version$Version, ' built on ', local_version$Date)
-  packageStartupMessage('To see the user guides use: browseVignettes("wv").')
-}

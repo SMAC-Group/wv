c3529d9361585da9bb65dbc861c2add11ec8c454
Lionel Voirol <lionelvoirol@hotmail.com>
2023-08-29 19:56:35 +0200

    added the tag _PACKAGE in wv-package.R for CRAN and add .github folder in .Rbuildignore

--- R/wv-package.R
+++ R/wv-package.R
@@ -7,4 +7,5 @@
 #' @description Provides a series of tools to compute and plot quantities related to classical and robust wavelet variance for time series and regular lattices. More details can be found, for example, in Serroukh, A., Walden, A.T., & Percival, D.B. (2000) <doi:10.2307/2669537> and Guerrier, S. & Molinari, R. (2016) <arXiv:1607.05858>.  
 #' @name wv
 #' @docType package
+"_PACKAGE"
 NULL
